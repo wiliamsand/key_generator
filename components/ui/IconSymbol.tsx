cb93285a958498f66b79c3e93f23ce7283e2aec5
Wiliam Sand <wiliamfs123@gmail.com>
2025-03-28 16:34:12 -0300

    versao expo e icones

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -13,6 +13,7 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'key.fill': 'vpn-key'
 } as Partial<
   Record<
     import('expo-symbols').SymbolViewProps['name'],
